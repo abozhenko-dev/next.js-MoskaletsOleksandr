b9d207cb357f612505efac96943900d7f327bac5
MoskaletsOleksandr <moskaletzsanych@gmail.com>
2023-11-27 15:23:53 +0200

    backup-before-eslint-configuration

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,22 +1,22 @@
-import { Meta } from '@components/index';
-import { MainLayout } from '@layouts/index';
-import { Contacts } from '@pages/index';
-import { NextPageWithLayout } from '@utils/index';
-import { ReactElement } from 'react';
+import { ReactElement } from "react";
+
+import { MainLayout } from "@layouts/index";
+
+import { Contacts } from "@pages";
+
+import { Meta } from "@components/index";
+
+import { NextPageWithLayout } from "@utils/index";
 
 const Page: NextPageWithLayout = () => {
   return (
     <>
-      <Meta meta={{ title: 'Contacts | Next.js' }} />
+      <Meta meta={{ title: "Contacts | Next.js" }} />
       <Contacts />
     </>
   );
 };
 
-Page.getLayout = (page: ReactElement) => (
-  <MainLayout>
-    {page}
-  </MainLayout>
-);
+Page.getLayout = (page: ReactElement) => <MainLayout>{page}</MainLayout>;
 
 export default Page;
